0e03dd022883184f4e307f408d93029690f04f4d
Mory Kwon <morykwon@Moryui-MacBookPro.local>
2020-09-28 23:27:37 +0900

    add problems programmers 두개뽑아서더하기 by js

---
+++ codes/javascript/src/programmers/두개뽑아서더하기/p1.test.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+  const set = new Set();
+
+  for (let i = 0; i < numbers.length - 1; i += 1) {
+    for (let j = i + 1; j < numbers.length; j += 1) {
+      set.add(numbers[i] + numbers[j]);
+    }
+  }
+
+  return [...set.values()].sort((a, b) => a - b);
+}
+
+test.each([
+  [[2, 1, 3, 4, 1], [2, 3, 4, 5, 6, 7]],
+  [[5, 0, 2, 7], [2, 5, 7, 9, 12]],
+])('두개뽑아서더하기', (n, ans) => {
+  expect(solution(n)).toEqual(ans);
+});
